a69c2fbf5f6d3b3c5e0f1a2a35e6fcfd39daeb28
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-10 14:28:15 +0200

    Connected first component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+  import Header from './components/HeaderApp.vue';
 
+  export default{
+    components:{
+      Header,
+    }
+  }
 </script>
 
 <template>
-
+  <Header/>
 </template>
 
 <style scoped>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,24 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        HEADER
+    </h1>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables.scss' as *;
+    @use '../styles/general' as *;
+
+    h1 {
+        color: $primary;
+    }
+</style>
